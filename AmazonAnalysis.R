046409e75376b0dc8df97b0585b11eb89d182ad0
hanct249 <70663273+hanct249@users.noreply.github.com>
2025-10-09 14:23:09 -0600

    added logistic regression model

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -7,7 +7,8 @@ library(skimr)
 
 #1050 columns
 
-trainData <- vroom("train.csv")
+trainData <- vroom("train.csv") %>%
+  mutate(ACTION=factor(ACTION))
 testData <- vroom("test.csv")
 skim(trainData)
 ggplot(data=trainData, aes(x=ACTION,y=RESOURCE)) +
@@ -23,4 +24,19 @@ amazonRecipe <- recipe(ACTION~., data=trainData) %>%
 prepped <- prep(amazonRecipe)
 baked <- bake(prepped, new_data=trainData)
   
+logRegModel <- logistic_reg() %>%
+  set_engine("glm")
+
+logRegwf <- workflow() %>%
+  add_recipe(amazonRecipe) %>%
+  add_model(logRegModel) %>%
+  fit(data=trainData)
+  
+amazon_predictions <- predict(logRegwf, new_data=testData, type="prob")
+
+kaggle_submission <- amazon_predictions %>%
+  select(.pred_1)
+
+vroom_write(x=kaggle_submission, file="./logPreds.csv", delim=",")
+
   
